f1c9b5b6f4e98e03e3859f50d9d1447fa6e2200e
Antônio Carlos <155235833+AntonioCarlos2501@users.noreply.github.com>
2023-12-30 10:38:38 -0300

    Update connection.py

--- data/connection.py
+++ data/connection.py
@@ -5,6 +5,9 @@ import psycopg2
 #PostgreSQL database
 # Create a new PostgreSQL database connection
 
+#Create a database 
+#Insert a new table into the database with the columns 'group_name', 'idgroup', 'username', 'userid', 'reaction_type'
+
 def connect_database(  ):
     return psycopg2.connect( database="database_name", user="user_name", password="#####", host="localhost", port="5432" )
 
